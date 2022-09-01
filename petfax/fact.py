b129507e61e4473548c870cef0eb98b438d4f907
valgarciav <vkgarcia@gmail.com>
2022-08-31 20:50:10 -0400

    facts/new
    working

--- petfax/fact.py
+++ petfax/fact.py
@@ -3,13 +3,10 @@ from flask import (Blueprint, render_template, request, redirect)
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['POST'])
 def index():
-    if request.method == 'POST':
-        print(request.form)
-        return redirect('/facts')
-
-    return render_template('facts/index.html')
+    print(request.form)
+    return 'Thanks for submitting a fun fact!'
 
 
 @bp.route('/new')
